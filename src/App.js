ad89172216cade73b9c45e13a24a48b8fa00ccb0
iaurg <italoaurelior@gmail.com>
2020-04-25 08:57:18 -0300

    :sparkles: List repositories from API

--- src/App.js
+++ src/App.js
@@ -22,13 +22,15 @@ function App () {
   return (
     <div>
       <ul data-testid='repository-list'>
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+            <button onClick={() => handleRemoveRepository(1)}>
             Remover
-          </button>
-        </li>
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
